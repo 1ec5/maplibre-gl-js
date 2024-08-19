45dca2b04030ce6f71da16dae6d9b64bd9e6522f
Minh Nguyễn <mxn@1ec5.org>
2024-08-18 19:03:28 -0700

    Fixed clipping of wide glyphs
    Increased the buffer around locally rendered glyphs.

--- src/render/glyph_manager.ts
+++ src/render/glyph_manager.ts
@@ -218,7 +218,7 @@ export class GlyphManager {
 
         return new GlyphManager.TinySDF({
             fontSize: 24 * textureScale,
-            buffer: 3 * textureScale,
+            buffer: 8 * textureScale,
             radius: 8 * textureScale,
             cutoff: 0.25,
             fontFamily: fontFamily,
