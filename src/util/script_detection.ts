1d58a0402f6de9bd4848722978344d0ae8c83a71
Minh Nguyễn <mxn@1ec5.org>
2024-08-25 01:41:35 -0700

    Streamlined property escape regular expressions

--- src/util/script_detection.ts
+++ src/util/script_detection.ts
@@ -75,7 +75,7 @@ function sanitizedRegExpFromScriptCodes(scriptCodes: Array<string>): RegExp {
             return null;
         }
     }).filter(pe => pe);
-    return new RegExp(supportedPropertyEscapes.join('|'), 'u');
+    return new RegExp(`[${supportedPropertyEscapes.join('')}]`, 'u');
 }
 
 /**
